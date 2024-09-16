e78eba9664095667557f071e01a46ccb93953ab1
DaiRodri <daianarodri@gmail.com>
2024-09-16 03:06:33 -0300

    Add files via upload

---
+++ questao1.js
@@ -0,0 +1,12 @@
+//Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
+var x = 1
+
+while (x <=100){
+
+if(x % 2 == 0){
+console.log (" os numeres pares são: " + x)
+   
+}
+x++
+}
---
+++ questão2.js
@@ -0,0 +1,12 @@
+//Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+var x = 1
+
+while (x <=1000){
+
+if(x % 2 !== 0){
+console.log (" os numeres pares são: " + x)
+   
+}
+x++
+}
